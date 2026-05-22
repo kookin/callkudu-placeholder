5eca459efedb01b9d66c6c99ef0de99ee9c37bc1
Robert Frantl <rfrantl@icloud.com>
2026-05-22 16:55:53 +0200

    add priv and terms

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        privacy: './privacy.html',
+        terms: './terms.html',
+      },
+    },
   },
 });
